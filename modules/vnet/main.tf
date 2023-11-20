44c3466af6dadc5296919b916b376e0c35cc4740
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-11-20 18:01:24 -0300

    nsg

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -18,6 +18,20 @@ resource "azurerm_network_security_group" "default" {
   resource_group_name = var.resource_group_name
 }
 
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "allow-ssh"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.default.name
+}
+
 resource "azurerm_subnet_network_security_group_association" "default" {
   subnet_id                 = azurerm_subnet.default.id
   network_security_group_id = azurerm_network_security_group.default.id
