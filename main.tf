819504ce56d7b4a7dee6569e769d80bf73dd6e0f
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-11-22 21:40:44 -0300

    variables

--- main.tf
+++ main.tf
@@ -38,9 +38,9 @@ module "mssql" {
   group    = azurerm_resource_group.default.name
   location = azurerm_resource_group.default.location
 
-  sku                           = "Basic"
-  max_size_gb                   = 2
   public_ip_address_to_allow    = var.public_ip_address_to_allow
+  sku                           = var.mssql_sku
+  max_size_gb                   = var.mssql_max_size_gb
   public_network_access_enabled = var.mssql_public_network_access_enabled
   admin_admin                   = var.mssql_admin_login
   admin_login_password          = var.mssql_admin_login_password
--- variables.tf
+++ variables.tf
@@ -3,13 +3,22 @@ variable "location" {
   default = "eastus2"
 }
 
+# Virutal Machine
 variable "vm_size" {
-  type    = string
-  default = "Standard_B2pts_v2"
+  type = string
 }
 
+# Azure SQL Server
 variable "public_ip_address_to_allow" {
   type = string
+}
+
+variable "mssql_sku" {
+  type = string
+}
+
+variable "mssql_max_size_gb" {
+  type = number
 }
 
 variable "mssql_public_network_access_enabled" {
