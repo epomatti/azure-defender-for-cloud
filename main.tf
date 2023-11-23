6821833c1f7877d6653482a1e29727912595e124
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-11-22 21:37:18 -0300

    mssql

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 locals {
-  workload = "bigenterprise"
+  workload = "bigbank79"
 }
 
 resource "azurerm_resource_group" "default" {
@@ -16,18 +16,32 @@ resource "azurerm_resource_group" "default" {
   location = var.location
 }
 
-module "vnet" {
-  source              = "./modules/vnet"
-  workload            = local.workload
-  resource_group_name = azurerm_resource_group.default.name
-  location            = azurerm_resource_group.default.location
-}
+# module "vnet" {
+#   source              = "./modules/vnet"
+#   workload            = local.workload
+#   resource_group_name = azurerm_resource_group.default.name
+#   location            = azurerm_resource_group.default.location
+# }
 
-module "vm" {
-  source              = "./modules/vm"
-  workload            = local.workload
-  resource_group_name = azurerm_resource_group.default.name
-  location            = azurerm_resource_group.default.location
-  subnet_id           = module.vnet.subnet_id
-  size                = var.vm_size
+# module "vm" {
+#   source              = "./modules/vm"
+#   workload            = local.workload
+#   resource_group_name = azurerm_resource_group.default.name
+#   location            = azurerm_resource_group.default.location
+#   subnet_id           = module.vnet.subnet_id
+#   size                = var.vm_size
+# }
+
+module "mssql" {
+  source   = "./modules/mssql"
+  workload = local.workload
+  group    = azurerm_resource_group.default.name
+  location = azurerm_resource_group.default.location
+
+  sku                           = "Basic"
+  max_size_gb                   = 2
+  public_ip_address_to_allow    = var.public_ip_address_to_allow
+  public_network_access_enabled = var.mssql_public_network_access_enabled
+  admin_admin                   = var.mssql_admin_login
+  admin_login_password          = var.mssql_admin_login_password
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "vm_size" {
   type    = string
   default = "Standard_B2pts_v2"
 }
+
+variable "public_ip_address_to_allow" {
+  type = string
+}
+
+variable "mssql_public_network_access_enabled" {
+  type = bool
+}
+
+variable "mssql_admin_login" {
+  type = string
+}
+
+variable "mssql_admin_login_password" {
+  type = string
+}
